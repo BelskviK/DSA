48ac99e48ae6ace79025d766fb7e0d12bd1054a0
Vano Gogebashvili <122786020+BelskviK@users.noreply.github.com>
2025-03-20 02:26:37 +0400

    Time: 2 ms (93.82%) | Memory: 57.9 MB (45.17%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -1,42 +1,33 @@
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number}
- */
- var findMedianSortedArrays = function (nums1, nums2) {
-  if (nums1.length > nums2.length) {
-    return findMedianSortedArrays(nums2, nums1);
-  }
 
-  let x = nums1.length;
-  let y = nums2.length;
-  let low = 0,
-    high = x;
-
-  while (low <= high) {
-    let partitionX = Math.floor((low + high) / 2);
-    let partitionY = Math.floor((x + y + 1) / 2) - partitionX;
-
-    let maxLeftX = partitionX === 0 ? -Infinity : nums1[partitionX - 1];
-    let minRightX = partitionX === x ? Infinity : nums1[partitionX];
-
-    let maxLeftY = partitionY === 0 ? -Infinity : nums2[partitionY - 1];
-    let minRightY = partitionY === y ? Infinity : nums2[partitionY];
-
-    if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
-      if ((x + y) % 2 === 0) {
-        return (
-          (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2.0
-        );
-      } else {
-        return Math.max(maxLeftX, maxLeftY);
-      }
-    } else if (maxLeftX > minRightY) {
-      high = partitionX - 1;
-    } else {
-      low = partitionX + 1;
+var findMedianSortedArrays = function(nums1, nums2) {
+   if (nums1.length > nums2.length) {
+        [nums1, nums2] = [nums2, nums1]; // Asegurar que nums1 sea el más corto
     }
-  }
 
-  throw new Error("Input arrays are not sorted");
+    let x = nums1.length, y = nums2.length;
+    let low = 0, high = x;
+
+    while (low <= high) {
+        let partitionX = Math.floor((low + high) / 2);
+        let partitionY = Math.floor((x + y + 1) / 2) - partitionX;
+
+        let maxX = (partitionX === 0) ? -Infinity : nums1[partitionX - 1];
+        let minX = (partitionX === x) ? Infinity : nums1[partitionX];
+
+        let maxY = (partitionY === 0) ? -Infinity : nums2[partitionY - 1];
+        let minY = (partitionY === y) ? Infinity : nums2[partitionY];
+
+        if (maxX <= minY && maxY <= minX) {
+            if ((x + y) % 2 === 0) {
+                return (Math.max(maxX, maxY) + Math.min(minX, minY)) / 2;
+            } else {
+                return Math.max(maxX, maxY);
+            }
+        } else if (maxX > minY) {
+            high = partitionX - 1;
+        } else {
+            low = partitionX + 1;
+        }
+    }
+
 };
