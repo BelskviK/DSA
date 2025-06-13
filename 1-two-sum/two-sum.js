92adeb4896dc8ac5052a9c3c16cfefb1825b5e1b
Vano Gogebashvili <122786020+BelskviK@users.noreply.github.com>
2025-06-13 13:01:47 +0400

    Time: 134 ms (5.45%) | Memory: 53.6 MB (92.23%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,15 +4,18 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const map = new Map();
-
-
-    for (let i = 0; i<nums.length;i++){
-        let secPair = target - nums[i];
-        if(map.has(secPair)){
-            return [map.get(secPair),i];
-        }
-        map.set(nums[i],i);
-    }
-        return []
+    let indexes=[];    
+    for(let i=0; i<nums.length; i++){
+        indexes[0]=i
+        for(let j=i+1; j<nums.length; j++){
+            indexes[1]=j ; 
+            if(( nums[indexes[0]] + nums[indexes[1]] ) === target){ 
+                break;
+            };
+        }; 
+        if(( nums[indexes[0]] + nums[indexes[1]] ) === target){ 
+            break;
+        };
+    }; 
+    return indexes;
 };
