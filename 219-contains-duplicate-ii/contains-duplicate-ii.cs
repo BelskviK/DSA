6b74ff1615891d3a3b20d6badc6f082deb1eaaea
Vano Gogebashvili <122786020+BelskviK@users.noreply.github.com>
2025-08-05 00:06:32 +0400

    Time: 46 ms (24.38%) | Memory: 69.6 MB (82.29%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public bool ContainsNearbyDuplicate(int[] nums, int k) {
+        HashSet<int> set = new HashSet<int>();
+
+        for (int i = 0; i < nums.Length; i++) {
+            if (set.Contains(nums[i])) {
+                return true;
+            }
+
+            set.Add(nums[i]);
+
+            // Maintain a sliding window of size k
+            if (set.Count > k) {
+                set.Remove(nums[i - k]);
+            }
+        }
+
+        return false;
+    }
+}
