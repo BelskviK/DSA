5dbbdeeedc56d68d483f95328959b7e33c389ed1
Vano Gogebashvili <122786020+BelskviK@users.noreply.github.com>
2025-08-15 00:09:18 +0400

    Time: 0 ms (100.00%) | Memory: 55.5 MB (6.22%) - LeetSync

---
+++ 100-same-tree/same-tree.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    if (!p && !q) return true;           // both null
+    if (!p || !q) return false;          // one is null, other isn't
+    if (p.val !== q.val) return false;   // values differ
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
